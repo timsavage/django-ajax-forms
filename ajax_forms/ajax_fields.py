eb498737f2ddb4f7ab7926cdc7775df5927de924
Tim Savage <tim.savage@jooceylabs.com>
2010-06-28 23:47:31 +1000

    Updated AJAX fields to work with Django 1.2

--- ajax_forms/ajax_fields.py
+++ ajax_forms/ajax_fields.py
@@ -1,35 +1,54 @@
 import re
+from decimal import Decimal
 
 from django import forms
+from django.core import validators
 from django.utils.datastructures import SortedDict
 from django.utils.translation import ugettext as _
 
 
+__all__ = (
+    'AlreadyRegistered', 'register', 'factory',
+    'AjaxField', 'AjaxDateField', 'AjaxDateTimeField', 
+    'AjaxDecimalField', 'AjaxEmailField', 'AjaxFloatField', 
+    'AjaxIntegerField', 'AjaxNumericField', 'AjaxTimeField'
+)
+
+
 class AlreadyRegistered(Exception):
-    "An attempt was made to register a form field more than once"
+    """
+    An attempt was made to register a form field more than once
+    """
     pass
 
 
+# Registration and creation of fields
+
 registry = {}
 
-
 def register(field, ajax_field):
-    "Register an ajax_field for a field"
+    """
+    Register an ajax_field for a field
+    """
     if field in registry:
         raise AlreadyRegistered(
-            _('The form field %s has already been registered.') %
-                field.__name__)
+            _('The form field %s has already been registered.') % field.__name__)
     registry[field] = ajax_field
 
-
 def factory(field_instance):
-    "Get an ajax_field instance for a field instance"
+    """
+    Get an ajax_field instance for a field instance
+    """
     ajax_field = registry.get(type(field_instance), AjaxField)
     return ajax_field(field_instance)
 
 
+# Built in fields
+
 class AjaxField(object):
-    "Base field mask"
+    """
+    Base field mask
+    """
 
     def __init__(self, field_instance):
         self.field = field_instance
@@ -37,11 +56,15 @@ class AjaxField(object):
         self._error_messages = {}
 
     def add_error_message(self, msg_key):
-        "Add an error message to be sent to client"
+        """
+        Add an error message to be sent to client
+        """
         self._error_messages[msg_key] = self.field.error_messages.get(msg_key)
 
     def add_rule(self, rule_name, value=None, msg_key=None, cast=None):
-        "Add a validation rule"
+        """
+        Add a validation rule
+        """
         if msg_key is None:
             msg_key = rule_name
         if value is None:
@@ -54,10 +77,29 @@ class AjaxField(object):
                 self._rules[rule_name] = value
 
     def parse(self):
-        "Hook for doing any extra parsing in sub class"
-        pass
+        """
+        Hook for doing any extra parsing in sub class
+        """
+        # Parse validator array
+        for validator in self.field.validators:
+            if isinstance(validator, validators.BaseValidator):
+                value = validator.limit_value
+                if isinstance(value, Decimal):
+                    value = str(value)
+                self.add_rule(validator.code, value)
+            elif isinstance(validator, validators.RegexValidator):
+                # Handle regex flags
+                flags = []
+                if validator.regex.flags & re.IGNORECASE:
+                    flags.append('i')
+                if validator.regex.flags & re.MULTILINE:
+                    flags.append('m')
+                self.add_rule('regex', (validator.regex.pattern, ''.join(flags)), 'invalid')        
 
     def to_ajax(self):
+        """
+        Convert field rules into json
+        """
         if self._rules is None:
             self._rules = SortedDict()
             if self.field.required:
@@ -72,52 +114,15 @@ class AjaxField(object):
 
 # These fields require nothing specialised they will be assigned AjaxField by
 # default in the factory method:
-#   BooleanField, NullBooleanField, ChoiceField, MultipleChoiceField,
-#   TypedChoiceField
+#   CharField, EmailField, IPAddressField, RegexField, URLField BooleanField, NullBooleanField, 
+#   ChoiceField, MultipleChoiceField, TypedChoiceField
 #
 # File fields have limited capability for validation on the client side and
 # will be treated the same as the above fields, these include:
 #   FileField, FilePathField, ImageField
 
-
-class AjaxCharField(AjaxField):
-
-    def parse(self):
-        super(AjaxCharField, self).parse()
-        self.add_rule('max_length')
-        self.add_rule('min_length')
-
-register(forms.CharField, AjaxCharField)
-
-
-class AjaxRegexField(AjaxCharField):
-
-    def parse(self):
-        super(AjaxRegexField, self).parse()
-        # Handle regex flags
-        flags = []
-        if self.field.regex.flags & re.IGNORECASE:
-            flags.append('i')
-        if self.field.regex.flags & re.MULTILINE:
-            flags.append('m')
-        self.add_rule('regex', (self.field.regex.pattern, ''.join(flags)), 'invalid')
-
-register(forms.RegexField, AjaxRegexField)
-register(forms.IPAddressField, AjaxRegexField)
-register(forms.URLField, AjaxRegexField)
-register(forms.EmailField, AjaxRegexField)
-
-
-class AjaxNumericField(AjaxField):
-
-    def parse(self):
-        super(AjaxNumericField, self).parse()
-        self.add_rule('max_value')
-        self.add_rule('min_value')
-
-
-class AjaxFloatField(AjaxNumericField):
-
+        
+class AjaxFloatField(AjaxField):
     def parse(self):
         self.add_rule('is_float', True, 'invalid')
         super(AjaxFloatField, self).parse()
@@ -126,7 +131,6 @@ register(forms.FloatField, AjaxFloatField)
 
 
 class AjaxDecimalField(AjaxField):
-
     def parse(self):
         self.add_rule('is_float', True, 'invalid')
         super(AjaxDecimalField, self).parse()
@@ -149,7 +153,7 @@ class AjaxDecimalField(AjaxField):
 register(forms.DecimalField, AjaxDecimalField)
 
 
-class AjaxIntegerField(AjaxNumericField):
+class AjaxIntegerField(AjaxField):
     def parse(self):
         self.add_rule('is_integer', True, 'invalid')
         super(AjaxIntegerField, self).parse()
@@ -158,7 +162,6 @@ register(forms.IntegerField, AjaxIntegerField)
 
 
 class AjaxDateField(AjaxField):
-
     def parse(self):
         super(AjaxDateField, self).parse()
         self.add_rule('is_date', True, 'invalid')
@@ -167,7 +170,6 @@ register(forms.DateField, AjaxDateField)
 
 
 class AjaxDateTimeField(AjaxField):
-
     def parse(self):
         super(AjaxDateTimeField, self).parse()
         self.add_rule('is_datetime', True, 'invalid')
@@ -176,7 +178,6 @@ register(forms.DateTimeField, AjaxDateTimeField)
 
 
 class AjaxTimeField(AjaxField):
-
     def parse(self):
         super(AjaxTimeField, self).parse()
         self.add_rule('is_time', True, 'invalid')
