f02fbd6424d8cb4deeaeabae21e6bd95a5f53f9f
Tim Savage <tim.savage@poweredbypenguins.org>
2012-02-21 10:41:54 +1100

    Updated version to 0.3.1

--- ajax_forms/__init__.py
+++ ajax_forms/__init__.py
@@ -1,5 +1,5 @@
 # Copyright 2009 Tim Savage <tim.savage@poweredbypenguins.org>
 # Licensed under the terms of the BSD License (see LICENSE)
 
-VERSION = (0, 3, 0, 'beta')
+VERSION = (0, 3, 1, 'beta')
 __version__ = '.'.join(map(str, VERSION))
